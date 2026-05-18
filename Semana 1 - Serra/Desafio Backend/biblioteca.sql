66befb9597d659fcdccb51d3591948d403d6fd12
jotakim <joaquimdinizbr10@gmail.com>
2026-05-17 23:29:46 -0300

    Correção do Desafio Backend

--- Semana 1 - Serra/Desafio Backend/biblioteca.sql
+++ Semana 1 - Serra/Desafio Backend/biblioteca.sql
@@ -1,31 +1,38 @@
-CREATE DATABASE IF NOT EXISTS biblioteca DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_unicode_ci;
+CREATE DATABASE IF NOT EXISTS biblioteca
+DEFAULT CHARSET=utf8
+DEFAULT COLLATE=utf8_unicode_ci;
 
 USE biblioteca;
 
-DROP TABLE IF EXISTS usuario;
-
-CREATE TABLE  usuario(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nome varchar(20) NOT NULL,
-  telefone varchar(9) NOT NULL,
-  email varchar(30) NOT NULL
-)ENGINE=INNODB; 
 
 DROP TABLE IF EXISTS emprestimo;
-
-CREATE TABLE emprestimo(
-  id INT AUTO_INCREMENT PRIMARY KEY, 
-  FkUsuario INT,
-  FkLivro INT,
-  datEmprestimo DATE,
-  FOREIGN KEY (FkUsuario) REFERENCES usuario(id),
-  FOREIGN KEY (FkUsuario) REFERENCES livro(id)
-);
-
 DROP TABLE IF EXISTS livro;
+DROP TABLE IF EXISTS usuario;
+
+
+CREATE TABLE usuario(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  telefone VARCHAR(20) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE
+) ENGINE=InnoDB;
+
 
 CREATE TABLE livro(
-  id INT AUTO_INCREMENT PRIMARY,
-  nome varchar(20),
-  autor varchar(20)
-);
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  autor VARCHAR(100) NOT NULL
+) ENGINE=InnoDB;
+
+
+CREATE TABLE emprestimo(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  fkUsuario INT NOT NULL,
+  fkLivro INT NOT NULL,
+  dataEmprestimo DATE NOT NULL,
+  dataDevolucao DATE,
+
+  FOREIGN KEY (fkUsuario) REFERENCES usuario(id),
+  FOREIGN KEY (fkLivro) REFERENCES livro(id)
+
+) ENGINE=InnoDB;
